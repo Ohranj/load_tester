32f61b95186fb674e8ceeae8db24846c500de17f
AlexD <Alex.Dorrington@qav-global.com>
2025-07-08 16:29:26 +0100

    WTD

--- flaskr/poll.py
+++ flaskr/poll.py
@@ -16,6 +16,8 @@ def index():
         'cpu_prcnt': psutil.cpu_percent(interval=1)
     }
 
+    #Read db
+
     jsonData = { 'success': True, 'message': 'Load retrieved', 'data': data, 'errors': [] }
     response = Response(jsonData, status = 200)
     return response.make_json_response()
